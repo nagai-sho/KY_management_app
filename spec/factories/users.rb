e5b09add27b7b83958095cae70ef1b479dd99cb6
Nagai_sho <as.uland.dr@gmail.com>
2024-09-27 09:36:26 +0900

    userテストコードの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    email                 { Faker::Internet.email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    company               { Faker::Company.name }
+    name                  { Faker::Name.name }
+    name_kana             { 'タナカ　タロウ' }
+    # phone_number          { Faker::PhoneNumber.cell_phone }
+    phone_number          { '090-1234-5678' }
+    position              { Faker::Job.position }
   end
 end
