0d35b68826fea980f545f84b24cc6aa399d58b09
Nagai_sho <as.uland.dr@gmail.com>
2024-09-27 17:51:39 +0900

    Projectの新規作成画面の実装

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,16 +1,31 @@
 class ProjectsController < ApplicationController
   def index
-    @site = Site.includes(:projects).find(params[:site_id])
+    @site = Site.find(params[:site_id])
     @projects = @site.projects
-    @project = @projects.first
-    @ky_sheet = @project.ky_sheets.first if @project.present?
-    # Rails.logger.debug "@site: #{@site.inspect}"
-    # Rails.logger.debug "@projects: #{@projects.inspect}"
-    # Rails.logger.debug "@project: #{@project.inspect}"
-    # Rails.logger.debug "@ky_sheet: #{@ky_sheet.inspect}"
   end
   def new
     @site = Site.find(params[:site_id])
     @project = @site.projects.new
+  end
+  
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to site_projects_path notice: '工事件名が正常に作成されました。'
+    else
+      flash.now[:alert] = '工事件名の作成に失敗しました。'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(
+      :name,
+      :client,
+      :construction_start,
+      :construction_completion,
+      :construction_content,
+      )
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
   has_many :ky_sheets
+
+  validates :name, presence: true
 end
