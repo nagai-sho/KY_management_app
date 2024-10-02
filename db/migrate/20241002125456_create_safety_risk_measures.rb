f713acb16e0553a59a9c0e8ac5d03e386e3136bf
Nagai_sho <as.uland.dr@gmail.com>
2024-10-02 22:04:46 +0900

    安全へのリスク低減対策の実装

---
+++ db/migrate/20241002125456_create_safety_risk_measures.rb
@@ -0,0 +1,10 @@
+class CreateSafetyRiskMeasures < ActiveRecord::Migration[7.0]
+  def change
+    create_table :safety_risk_measures do |t|
+      t.text :content, null: false
+      t.timestamps
+
+      t.references :site, null: false, foreign_key: true
+    end
+  end
+end
---
+++ spec/requests/safety_risk_measures_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "SafetyRiskMeasures", type: :request do
+
+end
