d0accb322b867068d8a506d79acde1b6d50d9c6b
Nagai_sho <as.uland.dr@gmail.com>
2024-10-04 17:26:04 +0900

    サイン機能（未実装）

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js" # @4.6.13
-pin "flatpickr/dist/l10n/ja", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/l10n/ja.js"
+pin "flatpickr" # @4.6.13
+pin "flatpickr/dist/l10n/ja", to: "flatpickr--dist--l10n--ja.js" # @4.6.13
+pin "signature_pad" # @5.0.3
