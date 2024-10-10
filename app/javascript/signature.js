7897a22306e737b2b7d6fabbd0051ab8c2989869
Nagai_sho <as.uland.dr@gmail.com>
2024-10-10 12:46:53 +0900

    signature_padの実装

--- app/javascript/signature.js
+++ app/javascript/signature.js
@@ -1,1 +1,8 @@
 console.log('Hello! javascript.');
+
+document.querySelectorAll('.signature-pad').forEach((canvas) => {
+  const signaturePad = new SignaturePad(canvas, {
+    backgroundColor: 'rgb(255, 255, 255)',
+    penColor: 'rgb(0, 0, 0)',
+  });
+});
