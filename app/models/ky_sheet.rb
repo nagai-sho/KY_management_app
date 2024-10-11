b0654445abddb6119db9f06cd833912003e3fe99
Nagai_sho <as.uland.dr@gmail.com>
2024-10-11 16:52:41 +0900

    一時保存

--- app/models/ky_sheet.rb
+++ app/models/ky_sheet.rb
@@ -3,9 +3,9 @@ class KySheet < ApplicationRecord
   belongs_to :user
   belongs_to :project
 
-  belongs_to :risk_level
-  belongs_to :incidence_rate
-  belongs_to :construction_type
+  belongs_to :risk_level,              optional: true
+  belongs_to :incidence_rate,          optional: true
+  belongs_to :construction_type,       optional: true
   
   belongs_to :site_risk_prediction,    optional: true
   belongs_to :site_risk_measure,       optional: true
@@ -19,4 +19,32 @@ class KySheet < ApplicationRecord
 
   validates :risk_level_id, :incidence_rate_id, :construction_type_id,
             numericality: { other_than: 0, message: "未選択項目を選択してください" }
+
+  # 一時的なデータ処理のための属性
+  attr_accessor :construction_completion,
+                :site_risk_prediction_id,
+                :site_risk_measure_id,
+                :safety_risk_prediction_id,
+                :safety_risk_measure_id,
+                :quality_risk_prediction_id,
+                :quality_risk_measure_id,
+                :location_id,
+                :construction_content_id,
+                :answer_1,
+                :answer_2,
+                :answer_3,
+                :answer_4,
+                :answer_5,
+                :answer_6,
+                :sig1,
+                :sig2,
+                :sig3,
+                :sig4,
+                :sig5,
+                :sig6,
+                :sig7,
+                :sig8,
+                :sig9,
+                :sig10,
+                :sig11
 end
