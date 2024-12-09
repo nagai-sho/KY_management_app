01956774b155b3bcc1d6223837be37222b96d765
Nagai_sho <as.uland.dr@gmail.com>
2024-12-09 20:57:32 +0900

    modelの修正

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,9 @@ class Project < ApplicationRecord
   has_many :ky_sheets, dependent: :destroy
 
   validates :name, presence: true
+
+  def self.search(keyword)
+    where('name LIKE :keyword OR client LIKE :keyword 
+    OR construction_content LIKE :keyword', keyword: "%#{keyword}%")
+  end
 end
