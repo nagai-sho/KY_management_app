9fe3360c94ccb1c524d5ec8dadcb6b4ca6716bfa
Nagai_sho <as.uland.dr@gmail.com>
2024-12-14 16:36:54 +0900

    検索機能の解除の実装

--- app/javascript/controllers/search_reload_controller.js
+++ app/javascript/controllers/search_reload_controller.js
@@ -1,7 +1,26 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="search-reload"
 export default class extends Controller {
   connect() {
+    console.log('search_reload.js');
+    const url = new URL(window.location.href);
+    const params = url.searchParams;
+    params.delete('search');
+    history.replaceState({}, document.title, url.pathname);
+    // this.searchClear();
   }
+
+  // searchClear() {
+  //   window.onload = function () {
+  //     // 現在のURLを取得
+  //     const url = new URL(window.location.href);
+  //     // URLSearchParamsオブジェクトを取得
+  //     const params = url.searchParams;
+  //     // 'search'パラメータを削除
+  //     params.delete('search');
+  //     // アドレスバーのURLからGETパラメータを削除
+  //     history.replaceState({}, document.title, url.pathname);
+  //   };
+  // }
 }
